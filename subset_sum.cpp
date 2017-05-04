ab5c92eff56a62ada65f6585366c977fc0081e97
whatPanache <pwcarney@bu.edu>
2017-05-03 21:05:47 -0400

    Setup basic functions

---
+++ subset_sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+bool subset_sum(int input[])
+{
+    return true;
+}
+
+int main()
+{
+    int input []= {1, 2, 3};
+    std::cout << subset_sum(input);
+}
+
